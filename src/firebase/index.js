2f597de7a94f203dc618f947b21859177288a8bb
Jose Tandavala <jose.tandavala@gmail.com>
2020-01-09 11:44:54 +0100

    Update index.js

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,14 +3,14 @@ import firebase from "firebase/app";
 import "firebase/storage";
 
 var config = {
-  apiKey: "AIzaSyAixnYAj9x9tKPw5DfCnEMSzsdB5W1_uKo",
-  authDomain: "bomcaracter-ce053.firebaseapp.com",
-  databaseURL: "https://bomcaracter-ce053.firebaseio.com",
-  projectId: "bomcaracter-ce053",
-  storageBucket: "bomcaracter-ce053.appspot.com",
-  messagingSenderId: "701395526037",
-  appId: "1:701395526037:web:f4a1008c6e76ab267bd69c",
-  measurementId: "G-ZN9JCGVKWM"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 // Initialize Firebase
 firebase.initializeApp(config);
